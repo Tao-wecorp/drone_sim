5c207209fe78955ce98a25bf1d58c948e5a072ff
boringtao <xinghui.tao@wecorpindustries.com>
2020-04-16 22:05:50 +0100

    Add: openpose yaw angle

---
+++ src/drone_rl/scripts/helpers/openpose/objects.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python
+
+import cv2
+import os
+import rospy
+import rospkg
+import time
+from math import *
+
+rospack = rospkg.RosPack()
+openpose_folder = os.path.join(rospack.get_path("drone_rl"), "scripts/helpers/openpose/models/")
+net = cv2.dnn.readNetFromTensorflow(openpose_folder + "graph_opt.pb")
+# net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+# net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+nPoints = 18
+threshold = 0.1
+inputSize = 300
+
+class OpenPose():
+    def __init__(self):
+        self.goal = 0.0  # [angle]
+
+    def detect(self, cv_image):
+        frameWidth = cv_image.shape[1]
+        frameHeight = cv_image.shape[0]
+
+        net.setInput(cv2.dnn.blobFromImage(cv_image, 1.0, (inputSize, inputSize), (127.5, 127.5, 127.5), swapRB=True, crop=False))
+        output = net.forward()
+        output = output[:, :nPoints, :, :]
+
+        H = output.shape[2]
+        W = output.shape[3]
+
+        points = []
+        for i in range(nPoints):
+            probMap = output[0, i, :, :]
+            minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
+            x = (frameWidth * point[0]) / W
+            y = (frameHeight * point[1]) / H
+
+            if (prob > threshold):
+                points.append((int(x), int(y)))
+            else :
+                points.append(None)
+        return points
+
+    def yaw(self, position):
+        new_goal = degrees(atan(float(320-position[0])/(480-position[1])))
+        yaw = new_goal + self.goal
+        self.goal = yaw
+        return yaw
---
+++ src/drone_rl/scripts/yaw.py
@@ -0,0 +1,94 @@
+#! /usr/bin/env python
+
+import rospy
+from gazebo_msgs.msg import ModelState, ModelStates
+from gazebo_msgs.srv import SetModelState
+from sensor_msgs.msg import Image
+from std_msgs.msg import Float64
+from std_srvs.srv import Empty
+
+from tf.transformations import quaternion_from_euler
+from geometry_msgs.msg import Point, Pose, Quaternion, Twist
+
+from copy import deepcopy
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+
+from math import *
+import numpy as np
+import random
+import time
+
+from helpers.openpose import OpenPose
+openpose = OpenPose()
+x_fpv, y_fpv = [320, 480]
+
+pose = Pose()
+
+class Yaw(object):
+    def __init__(self):
+        rospy.init_node('yaw_node', anonymous=True)
+
+        self.img_sub = rospy.Subscriber("/drone/front_camera/image_raw",Image,self.camera_callback)
+        self.bridge_object = CvBridge()
+        self.frame = None
+        self.robot_position = None
+
+        # self.states_sub = rospy.Subscriber("/gazebo/model_states",ModelStates,self.states_callback)
+        # self.set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)    
+        # self.reset_simulation = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
+        # self.reset_simulation()
+
+        rate = rospy.Rate(30)
+        state_robot_msg = ModelState()
+        state_robot_msg.model_name = 'robot'
+        while not rospy.is_shutdown():
+            if self.frame is not None:
+                start_time = time.time()
+                frame = deepcopy(self.frame)
+                
+                points = openpose.detect(frame)
+                x_hip, y_hip = points[11]
+                yaw_angle = openpose.yaw([x_hip, y_hip])
+                print yaw_angle
+                
+                rospy.wait_for_service('/gazebo/set_model_state')
+                try:
+                    pose.position = self.robot_position
+                    pose.orientation = Quaternion(*quaternion_from_euler(0.0, 0.0, yaw_angle*pi/180))
+                    state_robot_msg.pose = pose
+                    self.set_state(state_robot_msg)
+                except rospy.ServiceException, e:
+                    print(e)
+
+                for i in range(len(points)):
+                    if points[i] is not None:
+                        frame = cv2.circle(frame, (int(points[i][0]), int(points[i][1])), 3, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
+                # frame = cv2.circle(frame, (int(x_hip), int(y_hip)), 3, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
+                frame = cv2.circle(frame, (int(x_fpv), int(y_fpv)), 10, (255, 0, 255), thickness=-1, lineType=cv2.FILLED)
+                cv2.imshow("", frame)
+                cv2.waitKey(1)
+
+                # print("%s seconds" % (time.time() - start_time))
+                time.sleep(round((time.time() - start_time), 1))
+            rate.sleep()
+    
+    def camera_callback(self,data):
+        try:
+            cv_img = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
+        except CvBridgeError as e:
+            print(e)
+        self.frame = cv_img
+
+    def states_callback(self,data):
+        self.robot_position = data.pose[2].position        
+
+def main():
+    try:
+        Yaw()
+    except KeyboardInterrupt:
+        pass
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
