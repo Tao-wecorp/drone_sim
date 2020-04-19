11e5affe29ffea156baaeee16abd606876463877
boringtao <xinghui.tao@wecorpindustries.com>
2020-04-19 09:02:37 +0100

    Add: rotate objects

--- src/drone_rl/scripts/rotate.py
+++ src/drone_rl/scripts/rotate.py
@@ -4,31 +4,30 @@ from math import *
 import time
 import rospy
 from geometry_msgs.msg import Pose, Quaternion, Twist
-from std_msgs.msg import Empty
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
+from helpers.control import Control
+control = Control()
+kp = 0.5
 
 class Rotate(object):
     def __init__(self):
         rospy.init_node('rotate_node')
         self.rate = rospy.Rate(30)
-        self.kp = 0.5
+
+        self.yaw_angle = 80
         self.yaw = 0.0
 
         rospy.Subscriber ('/drone/gt_pose', Pose, self.pose_callback)
         self._pub_cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
         self._move_msg = Twist()
-        self._pub_takeoff = rospy.Publisher('/drone/takeoff', Empty, queue_size=1)
-        self._takeoff_msg = Empty()
-        self._pub_land = rospy.Publisher('/drone/land', Empty, queue_size=1)
-        self._land_msg = Empty()
 
-        self.takeoff()
+        control.takeoff()
         rospy.on_shutdown(self.land)
 
         while not rospy.is_shutdown():
-            yaw = 30*pi/180
-            self._move_msg.angular.z = self.kp * (yaw - self.yaw)
+            yaw = self.yaw_angle*pi/180
+            self._move_msg.angular.z = kp * (yaw - self.yaw)
             self._pub_cmd_vel.publish(self._move_msg)
             self.rate.sleep()
 
@@ -37,21 +36,8 @@ class Rotate(object):
         roll, pitch, yaw = euler_from_quaternion([orientation.x, orientation.y, orientation.z, orientation.w])
         self.yaw = yaw
 
-    def takeoff(self):
-        rospy.loginfo('Taking off...')
-        i=0
-        while not i == 3:
-            self._pub_takeoff.publish(self._takeoff_msg)
-            time.sleep(1)
-            i += 1
-
     def land(self):
-        rospy.loginfo('Landing...')
-        i=0
-        while not i == 3:
-            self._pub_land.publish(self._land_msg)
-            time.sleep(1)
-            i += 1
+        control.land()
         
 def main():
     try:
